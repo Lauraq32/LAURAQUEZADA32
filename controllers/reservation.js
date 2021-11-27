a1e153ab65f336564a6c2935075733c26084dde8
Lauraq32 <88895396+Lauraq32@users.noreply.github.com>
2021-11-26 20:11:04 -0400

    corrections of error
    1- added scheduleJob instead of cronJob.
    2- better use of sendgrid.
    3- can receive email one hour before reservation, or instantly in the same hour but in the past.
    4- now is working with moments.

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -1,98 +1,119 @@
 const { response, request } = require("express");
 const Reservation = require("../models/reservation");
-const sgMAIL = require('@sendgrid/mail'); 
-const cron = require('node-cron')
+const sgMAIL = require("@sendgrid/mail");
+const schedule = require("node-schedule");
+const moment = require("moment");
 
-const reservationPost = async (req, res ) => {
-    const reservations = new Reservation({
-        email: req.body.email,
-        product: req.body.product,
-        startTime: req.body.startTime,
-        endTime: req.body.endTime
-       
-    });
-    reservations.save().then(result => { 
-        res.status(201).json({
-            email: result.email,
-            product: result.product,
-            startTime: req.body.startTime,
-            endTime: req.body.endTime
+const reservationPost = async (req, res) => {
+  console.log(
+    `startTime: ${moment(req.body.startTime).toDate()} 
+    endTime: ${moment(req.body.endTime).toDate()}`
+  );
+
+  const reservations = new Reservation({
+    email: req.body.email,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+  });
+  reservations
+    .save()
+    .then(async (result) => {
+      const date = moment(req.body.startTime).subtract(1, "hour").toDate();
+      console.log(`scheduled date: ${date}`);
+      console.log(`current date: ${moment().toDate()}`);
+
+      const duration = moment.duration(
+        moment(req.body.startTime).diff(moment())
+      );
+      const difference = duration.asMinutes();
+
+      const message = {
+        to: req.body.email,
+        from: "lauraq2348@gmail.com",
+        subject: "Reservation remainder",
+        text: "Remenber the time for your reservation",
+      };
+
+      if (difference > 60) {
+        schedule.scheduleJob(date, async () => {
+          await sendEmail(message);
         });
-        cron.schedule('1 52 4 18 November Thu', () => {
-            const message = {
-                to: req.body.email,
-                from: 'lauraq2348@gmail.com',
-                subject: 'Reservation',
-                text: 'Remember the time for your reservation',
-            };
-
-            sgMAIL.send(message)
-            .then(response => console.log('Email sent...'))
-            .catch(error => console.log('couldnt sent'));
-        }, {
-            timezone: 'America/Caracas',
-        })
-    }).catch(err => {
-        res.status(404).json({
-            error: err
-        });
-    });    
-};
-
-const reservationDelete = async(req, res = response) => {
-    const { id } = req.params;
-    await Reservation.findByIdAndDelete( id, { status: false } );
-    res.status(200).json({
-        message: 'reservation deleted',
-    });
-};
-
-const reservationGet = async(req = request, res = response) => {
-    const id = req.params.id;
-
-    Reservation.findOne({ _id: id })
-        .then(doc => {
-            console.log("from database", doc);
-            if (doc) {
-                res.status(200).json({
-                    reservation: doc,
-                });
-            } else {
-                res.status(404).json({message: "reservation not found"});
-            }
-        });
-};
-
-const reservationPut = async(req, res = response) => {
-    const id = req.params.id;
-
-    const updateOps = {
-        email: req.body.email,
-        product: req.body.product,
+      } else if (difference >= 0 && difference <= 60) {
+        await sendEmail(message);
+      }
+      res.status(201).json({
+        email: result.email,
         startTime: req.body.startTime,
         endTime: req.body.endTime,
-    };
-
-    Reservation.updateOne({ _id: id }, { $set: updateOps })
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'reservation updated',
-            });
-        })
-        .catch(err => {
-            res.status(400).json({
-                error: err
-            });
-        });
-
+      });
+    })
+    .catch((err) => {
+      console.log(`an error occurred ${err}`);
+      res.status(404).json({
+        error: err,
+      });
+    });
 };
 
+const sendEmail = async (message) => {
+  try {
+    await sgMAIL.send(message);
+    console.log("email successfully sent");
+  } catch (error) {
+    console.log(`error sending email ${error}`);
+  }
+};
+
+const reservationDelete = async (req, res = response) => {
+  const { id } = req.params;
+  await Reservation.findByIdAndDelete(id, { status: false });
+  res.status(200).json({
+    message: "reservation deleted",
+  });
+};
+
+const reservationGet = async (req = request, res = response) => {
+  const id = req.params.id;
+
+  Reservation.findOne({ _id: id }).then((doc) => {
+    console.log("from database", doc);
+    if (doc) {
+      res.status(200).json({
+        reservation: doc,
+      });
+    } else {
+      res.status(404).json({ message: "reservation not found" });
+    }
+  });
+};
+
+const reservationPut = async (req, res = response) => {
+  const id = req.params.id;
+
+  const updateOps = {
+    email: req.body.email,
+    startTime: req.body.startTime,
+    endTime: req.body.endTime,
+  };
+
+  Reservation.updateOne({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "reservation updated",
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        error: err,
+      });
+    });
+};
 
 module.exports = {
-    reservationPost,
-    reservationGet,
-    reservationPut,
-    reservationDelete,
-    
-}
+  reservationPost,
+  reservationGet,
+  reservationPut,
+  reservationDelete,
+  reservationGet
+};
